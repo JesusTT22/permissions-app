c8ed3b1e074686b853f9063ee29bb0d9080da30e
JesusTT22 <jesus22torrealba@gmail.com>
2026-04-30 17:30:09 -0400

    desarrollo de logica de administrador de permisos

--- permissions_app/doctype/administrar_permisos/administrar_permisos.js
+++ permissions_app/doctype/administrar_permisos/administrar_permisos.js
@@ -2,7 +2,7 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Administrar Permisos', {
-	// refresh: function(frm) {
-
-	// }
+	rol_principal: function(frm) {
+		
+	}
 });
--- permissions_app/doctype/administrar_permisos/administrar_permisos.py
+++ permissions_app/doctype/administrar_permisos/administrar_permisos.py
@@ -1,8 +1,25 @@
 # Copyright (c) 2026, Jesus T. and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class AdministrarPermisos(Document):
 	pass
+
+	@frappe.whitelist()
+	def get_documentos_rol(rol):
+
+		# Validar si se obtuvo el ID del rol.
+		if not rol:
+			return []
+
+		# Obtener el tipo de documento del rol.
+		document = frappe.get_all('Custom DocPerm', filters={
+			"role": rol,
+			"read": 1,
+		}, fields=["parent"])
+
+		clean_list = list(set([d.doctype_name for d in document]))
+
+		return [{"tipo_documento": doc} for doc in clean_list]
