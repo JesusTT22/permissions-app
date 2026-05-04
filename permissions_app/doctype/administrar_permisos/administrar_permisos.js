3eeeb79e36506d04ca5e8a95c6d4028198c3a84a
Jesus Torrealba <jesus22torrealba@gmail.com>
2026-05-04 13:10:46 -0400

    obtención de links de doctypes

--- permissions_app/doctype/administrar_permisos/administrar_permisos.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Jesus T. and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Administrar Permisos', {
-	rol_principal: function(frm) {
-		
-	}
-});
